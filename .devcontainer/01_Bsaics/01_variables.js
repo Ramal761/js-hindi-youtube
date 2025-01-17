49dd02caa1139cb1828cd1bf85173c0ad4198503
Ramal761 <ramalmurtaza@gmail.com>
2025-01-17 11:45:00 +0000

    node.js

---
+++ .devcontainer/01_Bsaics/01_variables.js
@@ -0,0 +1,33 @@
+const accountId=233390
+let accountEmail="ramalmurtaza@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2// not allowed
+
+console.log=(accountId);
+
+
+
+
+/* prefer not to use var
+because of issue in block scope and functional scope
+
+
+
+
+
+
+
+
+
+
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Bangluru"
+
+console.log=(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity
+])
